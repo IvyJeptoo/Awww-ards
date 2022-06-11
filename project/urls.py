fb3699be81721b934649562a50439631b9bba70a
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-06-11 16:45:13 +0300

    register application urls on the main url

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',include('awards.urls')),    
+    path('',include('awards.urls')),    
     path('', include('django.contrib.auth.urls'))
     
 ]
