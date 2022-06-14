77bd733fa1a6792fd86e768e0b8510ee53b97e4b
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-06-14 23:32:15 +0300

    register ratings in settings and in templates

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'awards',
     'bootstrap4',
+    'star_ratings'
     
 ]
 
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request',
             ],
         },
     },
--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('awards.urls')),    
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
+
     
 ]
